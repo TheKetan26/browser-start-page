bb6754a9617cdb33472a4085facf5ce41d6bbb49
Ketan Solanki <ketangsolanki129@gmail.com>
2023-06-14 12:52:47 +0530

    Solving issues releated to video

--- src/Background.js
+++ src/Background.js
@@ -1,18 +1,22 @@
 import './Background.css'
-import bg from './bg_pokemon.mp4'
+import ReactPlayer from 'react-player'
+// import bg from 'https://drive.google.com/uc?id=1Gz0AZP2OqF87T7W2l74YoIvea3BSiv8e'
 
 
 export default function Background() {
+    const video = document.getElementsByClassName('bg')
+
+    console.log(video)
+
+
     return (
         <section className = "bg">
-            <video
-                width = '100vw'
-                height = '100vh'
-                src = { bg }
-                loop
-                autoPlay
-            >
-            </video>
+            <iframe 
+                src="https://drive.google.com/file/d/1Gz0AZP2OqF87T7W2l74YoIvea3BSiv8e/preview" 
+                width="640" 
+                height="480" 
+                allow="autoplay"
+            ></iframe>
         </section>
     )
 };
